c21643e1e7b6d525bfec911c43a8cf79d06f049e
Sufyan5g <kalashahkala4455@gmail.com>
2025-07-29 22:15:41 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Automatically update the year in the footer
-    const yearSpan = document.getElementById('year');
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
-    }
-});
